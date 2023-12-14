ff57ab773353a480571189b5fcf25113c09df91a
mightgoyardstill <63618734+mightgoyardstill@users.noreply.github.com>
2023-12-14 13:10:14 +0000

    Update StandaloneFilterApp.cpp

--- shared/StandaloneFilterApp.cpp
+++ shared/StandaloneFilterApp.cpp
@@ -1,6 +1,5 @@
 #include "StandaloneFilterApp.h"
 
-//==============================================================================
 juce::JUCEApplicationBase* juce_CreateApplication()
 {
     return new StandaloneFilterApp();
